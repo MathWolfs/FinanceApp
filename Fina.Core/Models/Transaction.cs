ee76a1be15543a46265bae0633971c287b9fcc91
Matheus Chagas <matschagas@hotmail.com>
2025-07-01 09:52:10 -0300

    Add project files.

---
+++ Fina.Core/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace Fina.Core.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public String Title { get; set; } = String.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? PaidOrReceivedAt { get; set; }
+
+    }
+} 
